809a9354edbba13c874ee3975be70624eac0facb
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-18 21:52:02 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
+  attr_accessor :pets, :name
+  @@all = []
+
+  def initialize(pets)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def species
+    @species = "human"
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def name
+    @name = name
+  end
+
   # code goes here
 end
